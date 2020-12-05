74aa44247859966069b9bf61be6e5b2051a09284
aynur <galimovaynur@protonmail.com>
2020-12-06 00:58:29 +0500

    1. Читаем файл конфигурации [#420096]

---
+++ src/main/java/ru/job4j/io/Config.java
@@ -0,0 +1,49 @@
+package ru.job4j.io;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringJoiner;
+
+public class Config {
+    private final String path;
+    private final Map<String, String> values = new HashMap<>();
+
+    public Config(final String path) {
+        this.path = path;
+    }
+
+    public void load() {
+        try (BufferedReader in = new BufferedReader(new FileReader(path))) {
+            in.lines()
+                    .filter(line -> !line.startsWith("#"))
+                    .filter(line -> !line.isEmpty())
+                    .map(line -> line.split("="))
+                    .forEach(line -> values.put(line[0], line[1]));
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String value(String key) {
+        return values.get(key);
+//        throw new UnsupportedOperationException("Don't impl this method yet!");
+    }
+
+    @Override
+    public String toString() {
+        StringJoiner out = new StringJoiner(System.lineSeparator());
+        try (BufferedReader read = new BufferedReader(new FileReader(this.path))) {
+            read.lines().forEach(out::add);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return out.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Config("app.properties"));
+    }
+}
