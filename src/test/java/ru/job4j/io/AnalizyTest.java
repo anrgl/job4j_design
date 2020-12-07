d481f27a6e56903ad1fcca9d86f29487b2daf613
aynur <galimovaynur@protonmail.com>
2020-12-07 20:56:41 +0500

    3.0. Тестирование IO [#420088]

--- src/test/java/ru/job4j/io/AnalizyTest.java
+++ src/test/java/ru/job4j/io/AnalizyTest.java
@@ -1,9 +1,10 @@
 package ru.job4j.io;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.*;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -11,13 +12,26 @@ import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
 public class AnalizyTest {
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
+
     @Test
-    public void whenServerUnavailableTwoTimes() {
-        String serverLog = "./data/server.log";
-        String unavailable = "./data/unavailable.cvs";
+    public void whenServerUnavailableTwoTimes() throws IOException {
+        File source = folder.newFile("server.log");
+        File target = folder.newFile("unavailable.cvs");
         Analizy analizy = new Analizy();
-        analizy.unavailable(serverLog, unavailable);
-        try (BufferedReader in = new BufferedReader(new FileReader(unavailable))) {
+        try (PrintWriter out = new PrintWriter(source)) {
+            out.println("200 10:56:01");
+            out.println("500 10:57:01");
+            out.println("400 10:58:01");
+            out.println("200 10:59:01");
+            out.println("500 11:01:02");
+            out.println("200 11:02:02");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        analizy.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
+        try (BufferedReader in = new BufferedReader(new FileReader(target))) {
             List<String> lines = in.lines().collect(Collectors.toList());
             assertThat(lines.get(0), is("10:57:01;10:59:01"));
             assertThat(lines.get(1), is("11:01:02;11:02:02"));
